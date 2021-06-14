7c5a63db666938dc3ba408a4f06e7ab28dc07c4b
kaminee1jan <83804073+kaminee1jan@users.noreply.github.com>
2021-06-14 21:42:38 +0530

    Add files via upload

--- maximum_minimum.py
+++ maximum_minimum.py
@@ -1,13 +1,61 @@
-a=[1,2,3,4,5,6,7,8,9,10]
-i=0
-min_1=a[0]
-max_1=0
-while i<len(a):
-    if a[i]<min_1:
-        min_1=a[i]
-    if a[i]>max_1:
-        max_1=a[i]
-        i=i+1
-print(max_1)
-print(min_1)
+# a=[1,2,3,4,5,6,7,8,9,10]
+# i=0
+# min_1=a[0]
+# max_1=0
+# while i<len(a):
+#     if a[i]<min_1:
+#         min_1=a[i]
+#     if a[i]>max_1:
+#         max_1=a[i]
+#         i=i+1
+# print(max_1)
+# print(min_1)
 
+
+
+# list_1=[1,2,3,4,5,6,7,8,10]
+# max_1=0
+# i=0
+# while i<len(list_1):
+#     if list_1[i]>max_1:
+#         max_1=list_1[i]
+#     i=i+1
+# print(max_1)
+
+
+
+# color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# print(color[1:4])
+
+
+
+# numbers=[1,23,1,23,6,7,6,6]
+# index=0
+# sum=0
+# while index<len(numbers):
+#     sum=sum+numbers[index]
+#     index=index+1
+# print(sum)
+
+
+
+# name=input("enter the alphabet")
+# if name=="a" and "e" and "i" and "o" and "u" or name=="A" and "E" and "I" and "O" and "U":
+#     print("vowel")
+# else:
+#     print("consonant")
+
+
+
+
+
+
+
+# numbers=[1,23,1,23,6,7,6,6]
+# index=0
+# empty_list=[]
+# while index<len(numbers):
+#     if numbers[index] not in empty_list:
+#         empty_list.append(numbers[index])
+#     index=index+1
+# print(empty_list)
